ae1f9e039b29f2f607d4637b35f5cc5f58203c76
F1ankerB <136216045+F1ankerB@users.noreply.github.com>
2023-12-19 17:04:19 +1000

    register model in admin

--- rest_fstr/rest/admin.py
+++ rest_fstr/rest/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
+from .models import User, Coords, PerevalAdded, PerevalImages, PerevalAreas, SprActivitiesTypes
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('email', 'name', 'phone')
 
+class CoordsAdmin(admin.ModelAdmin):
+    list_display = ('latitude', 'longitude', 'height')
+
+class PerevalAddedAdmin(admin.ModelAdmin):
+    list_display = ('user', 'coord', 'beautyTitle', 'title', 'status')
+
+class PerevalImagesAdmin(admin.ModelAdmin):
+    list_display = ('pereval', 'date_added')
+
+class PerevalAreasAdmin(admin.ModelAdmin):
+    list_display = ('id_parent', 'title')
+
+class SprActivitiesTypesAdmin(admin.ModelAdmin):
+    list_display = ('title',)
+
+# Регистрация моделей
+admin.site.register(User, UserAdmin)
+admin.site.register(Coords, CoordsAdmin)
+admin.site.register(PerevalAdded, PerevalAddedAdmin)
+admin.site.register(PerevalImages, PerevalImagesAdmin)
+admin.site.register(PerevalAreas, PerevalAreasAdmin)
+admin.site.register(SprActivitiesTypes, SprActivitiesTypesAdmin)
 # Register your models here.
